47ec4d84ac9c159c75b4a663f91a702e5cf9a665
marat03 <57022105+marat03@users.noreply.github.com>
2020-02-26 17:35:22 +0300

    Add files via upload

---
+++ Project3/main.js
@@ -0,0 +1,79 @@
+const sections = $(".section");
+const display = $(".maincontent");
+
+let inscroll = false;
+
+const switchActiveClassInsideMenu = menuItemIndex => {
+    $('.fixed-menu__item').eq(menuItemIndex).addClass('active').siblings().removeClass('active');
+}
+
+
+const performTransition = sectionEq => {
+    if (inscroll) return
+
+    inscroll = true;
+
+    const position = sectionEq * -100 +'%';
+
+    sections
+        .eq(sectionEq)
+        .addClass("active")
+        .siblings()
+        .removeClass("active");
+
+    display.css({
+        transform : `translateY(${position})`
+    });
+
+    setTimeout(() =>{
+       inscroll = false;
+       switchActiveClassInsideMenu(sectionEq)
+    }, 1000 + 300);
+};     
+
+
+
+const scrollToSection = direction =>{
+    const activeSection = sections.filter('.active');
+    const nextSection = activeSection.next();
+    const prevSection = activeSection.prev();
+
+    if (direction === 'next' && nextSection.length) {
+        performTransition(nextSection.index());
+    }
+
+    if (direction === 'prev' && prevSection.length) {
+        performTransition(prevSection.index());
+    }
+}
+
+$('.wrapper').on('wheel', e =>{
+    const deltaY = e.originalEvent.deltaY;
+
+    if (deltaY > 0) {
+
+    scrollToSection('next');
+
+    }
+
+    if (deltaY < 0) {
+        
+    scrollToSection('prev');
+
+    }
+});
+
+$(document).on('keydown', e =>{
+
+    switch(e.keyCode) {
+        case 38:scrollToSection('prev'); break;
+        case 40:scrollToSection('next'); break;
+    }
+});
+
+$('[data-scroll-to]').on('click', e => {
+    e.preventDefault();
+    const target = $(e.currentTarget).attr('data-scroll-to');
+
+    performTransition(target);
+});
